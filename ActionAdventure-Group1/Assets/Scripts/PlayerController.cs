285aedb58d9ccddddf9a47b3aa735b461d1b37f2
tactless2004 <leyton.mckinney@gmail.com>
2025-11-04 16:18:22 -0600

    feat: add PlayerMove.cs
    `PlayerMove.cs` handles movement, as well as rotating the player with options to snap in 8-directions or 4-directions. There is also an rotation offset, so if the grid is rotated $x$ degrees from the world Z-axis, then we can fix the snapping with a parameter

--- ActionAdventure-Group1/Assets/Scripts/PlayerController.cs
+++ ActionAdventure-Group1/Assets/Scripts/PlayerController.cs
@@ -1,14 +1,14 @@
 /************************************************************
 * COPYRIGHT:  2025
-* PROJECT: New input system
+* PROJECT: AdventureGameNameTBA
 * FILE NAME: PlayerController.cs
 * DESCRIPTION: A player controller that uses the new input system to contol the movement of a player.
 *                   
 * REVISION HISTORY:
 * Date [YYYY/MM/DD] | Author | Comments
 * ------------------------------------------------------------
-* 2000/01/01 | Your Name | Created class
 * 2025/10/01 | Noah Zimmerman | Created Class
+* 2025/11/04 | Leyton McKinney | Modified to use PlayerMovement instead of MoveTransform
 *
 ************************************************************/
  
@@ -17,46 +17,28 @@ using UnityEngine.InputSystem;
  
 public class PlayerController : MonoBehaviour
 {
- 
+    private PlayerMove _playerMove;
     
-    //Reference to the MoveTransform component 
-    private MoveTransform _moveTransform;
-    
-    // Start is called once before the first Update
     private void Start()
     {
         // Check if MoveTransform component DOES NOT EXIST
-        if (!TryGetComponent<MoveTransform>(out _moveTransform))
+        if (!TryGetComponent<PlayerMove>(out _playerMove))
         {
-            Debug.LogError("MoveTransform component missing!");
+            Debug.LogError("PlayerMove component missing!");
 
-        }//end if(!TryGetComponent<MoveTransform>(out _moveTransform))
-    }//end Start()
+        }
+    }
  
- 
-    /// <summary>
-    /// Triggered by the Move input Action. Converts the 2D input from the player
-    /// (keyboard, joystick, or gamepad) into a 3D direction and passes it to the 
-    /// MoveTransform component to move the player.
-    /// </summary>
-    /// <param name="value">
-    /// The InputValue wrapper passed automatically by the Player Input component. 
-    /// </param>
+
     public void OnMove(InputValue value)
     {
-        // Extract the Vector2 from the InputValue
         Vector2 inputVector = value.Get<Vector2>();
-        Debug.Log("OnMove called: " + inputVector);
 
-        // Convert the 2D input into a 3D direction on the XZ plane
+        // Vec2 -> Vec3
         Vector3 direction = new Vector3(inputVector.x, 0f, inputVector.y);
-        Debug.Log("OnMove direction: " + direction);
+        _playerMove.Direction = direction;
         
-
-        // Call the movement component with the calculated direction
-        _moveTransform.Move(direction);
-        
-    } //end OnMove()
+    }
  
  
-}//end PlayerController
+}
---
+++ ActionAdventure-Group1/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,124 @@
+/************************************************************
+* COPYRIGHT:  Year
+* PROJECT: Name of Project or Assignment
+* FILE NAME: PlayerMove.cs
+* DESCRIPTION: Short Description of script.
+*                   
+* REVISION HISTORY:
+* Date [YYYY/MM/DD] | Author | Comments
+* ------------------------------------------------------------
+* 2025/11/04 | Leyton McKinney | Init
+*
+************************************************************/
+
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using System; // Need for Enum
+ 
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerMove : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    private enum SnapType
+    {
+        FourDirection,
+        EightDirection
+    }
+
+    [SerializeField]
+    private SnapType snapType = SnapType.FourDirection;
+
+    [SerializeField]
+    [Tooltip("If the isogrid is rotated, this will fix the snap")]
+    private float gridRotationOffsetY = 0.0f;
+
+    [SerializeField]
+    [Tooltip("Direction the player is facing. \nREADONLY DON'T MODIFY")]
+    private Vector3 facing;
+
+    [SerializeField]
+    private float MAX_FORCE = 10.0f;
+
+    [SerializeField]
+    private float _forceApplied;
+
+    [SerializeField]
+    private Vector3 _direction;
+
+    public float ForceApplied
+    {
+        get => _forceApplied;
+        set => _forceApplied = Mathf.Clamp(value, 0.0f, MAX_FORCE);
+    }
+
+    public Vector3 Direction
+    {
+        get => _direction;
+        set => _direction = value.normalized;
+    }
+    // Awake is called once on initialization (before Start)
+    private void Awake()
+    {
+        facing = Vector3.forward;
+        if(!TryGetComponent<Rigidbody>(out rb))
+        {
+            Debug.LogError("Rigidbody not found on Player!");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 currentVelocity = rb.linearVelocity;
+        Vector3 targetVelocity = Direction * ForceApplied;
+
+        // This creates some semblance of momentum, so the player can't change direction on a dime
+        // but, movement also doesn't feeling slippery. A good balance methinks.
+        Vector3 dv = new Vector3(
+            targetVelocity.x - currentVelocity.x,
+            0.0f,
+            targetVelocity.z - currentVelocity.z
+        );
+        rb.AddForce(dv, ForceMode.VelocityChange);
+
+        // Snap to direction
+        if(Direction != Vector3.zero) SnapDirection();
+    }
+
+    private void SnapDirection()
+    {
+        Vector3 snappedDir = Direction;
+        if (snapType == SnapType.FourDirection)
+        {
+            // Whichever Axis has a higher influence on the direction, we snap that way
+            if (Mathf.Abs(Direction.x) > Mathf.Abs(Direction.z))
+            {
+                // Snap x
+                snappedDir = new Vector3(Mathf.Sign(Direction.x), 0.0f, 0.0f);
+            }
+            else
+            {
+                // Snap z
+                snappedDir = new Vector3(0.0f, 0.0f, Mathf.Sign(Direction.z));
+            }
+        } 
+        
+        if (!Enum.IsDefined(typeof(SnapType), snapType))
+        {
+            Debug.LogError("No SnapType Chosen!\nDefaulting to Eight Direction.");
+        }
+
+        // fix local snappedDir to worldSnappedDir
+        Quaternion gridRotationOffset = Quaternion.Euler(0.0f, gridRotationOffsetY, 0.0f);
+        Vector3 worldSnappedDir = gridRotationOffset * snappedDir;
+        // Apply Snap
+        transform.rotation = Quaternion.LookRotation(worldSnappedDir);
+        facing = worldSnappedDir;
+    }
+}
