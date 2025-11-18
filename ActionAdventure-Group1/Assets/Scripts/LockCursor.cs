00a177e8af911b67c22a6b348ccdcc0283cea480
tactless2004 <leyton.mckinney@gmail.com>
2025-11-17 22:59:38 -0600

    fix: Cursor lock state is now unlocked/locked in the contexts where it should be.

--- ActionAdventure-Group1/Assets/Scripts/LockCursor.cs
+++ ActionAdventure-Group1/Assets/Scripts/LockCursor.cs
@@ -1,14 +1,14 @@
 /************************************************************
-* COPYRIGHT:  Year
-* PROJECT: Name of Project or Assignment
+* COPYRIGHT:  2025
+* PROJECT: The Hollow Castle
 * FILE NAME: LockCursor.cs
-* DESCRIPTION: Short Description of script.
+* DESCRIPTION: Manages cursor lock state.
 *                   
 * REVISION HISTORY:
 * Date [YYYY/MM/DD] | Author | Comments
 * ------------------------------------------------------------
-* 2000/01/01 | Your Name | Created class
-*
+* 2025/11/16 | Leyton McKinney | Init
+* 2025/11/17 | Leyton McKinney | Align cursor state with scene state.
 *
 ************************************************************/
 
@@ -18,18 +18,43 @@ using UnityEngine;
 
 public class LockCursor : MonoBehaviour
 {
-    private void Update()
+
+    private GameManager gameManager;
+
+    private void Awake()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if(!GameObject.FindGameObjectWithTag("GameManager").TryGetComponent(out gameManager)) {
+            Debug.LogError($"{name} could not find GameManager");
+        }
+
+        // When a level is loaded, the cursor should initially be locked.
+        // This might create issues down the line.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void OnEnable()
+    {
+        // Subscribe to GameState Publisher
+        gameManager.onGameStateChanged += SetCursorLockState;
+    }
+
+    private void OnDisable()
+    {
+        // Unsubscribe to GameState Publisher
+        gameManager.onGameStateChanged -= SetCursorLockState;
+    }
+
+    private void SetCursorLockState(GameState state)
+    {
+        // If in any State that is not game play, lock cursor.
+        // this is generally menus.
+        if (state != GameState.GamePlay)
         {
             Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
         }
-        
-        // Refocus on RMB
-        if (Input.GetMouseButtonDown(0) && Cursor.lockState == CursorLockMode.None) {
+        else
+        {
             Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
         }
     }
 }
