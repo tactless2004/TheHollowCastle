968f4a271250599fa4cb4224a4cc78071bd444d1
chaseacone <chasecone@proton.me>
2025-12-01 14:53:52 -0600

    Added the doors as well as a keys required field for them.

---
+++ ActionAdventure-Group1/Assets/Scripts/DoorMechanics.cs
@@ -0,0 +1,23 @@
+/************************************************************
+ * COPYRIGHT:  Year
+ * PROJECT: Name of Project or Assignment
+ * FILE NAME: PlayerKeys.cs
+ * DESCRIPTION: Short Description of script.
+ *
+ * REVISION HISTORY:
+ * Date [YYYY/MM/DD] | Author | Comments
+ * ------------------------------------------------------------
+ * 2000/01/01 | Your Name | Created class
+ * 2025/12/01 | Chase Cone | Created class
+ *
+ ************************************************************/
+
+using Unity.VisualScripting;
+using UnityEngine;
+ 
+
+public class DoorMechanics : MonoBehaviour
+{
+    public int keysRequired = 1;
+    
+}
