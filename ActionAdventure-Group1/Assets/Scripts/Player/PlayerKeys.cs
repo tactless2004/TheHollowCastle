2193ca8cbe1595ff5ce5d04adf3b1f9d5865b924
chaseacone <chasecone@proton.me>
2025-12-01 15:17:32 -0600

    Door moving up mechanics

--- ActionAdventure-Group1/Assets/Scripts/Player/PlayerKeys.cs
+++ ActionAdventure-Group1/Assets/Scripts/Player/PlayerKeys.cs
@@ -9,6 +9,7 @@
 * ------------------------------------------------------------
 * 2000/01/01 | Your Name | Created class
 * 2025/12/01 | Chase Cone | Created class
+ * 2025/12/01 | Chase Cone | Added door opening mechanics
 *
 ************************************************************/
 
@@ -18,8 +19,9 @@ using UnityEngine;
 
 public class PlayerKeys : MonoBehaviour
 {
-    [SerializeField] private int numKeys; 
- 
+    [SerializeField] private int numKeys;
+    [SerializeField] private float doorOpenSpeed = 1;
+    [SerializeField] private float openTime = 8;
  
     /// <summary>
     /// Detect when a player runs into something.
@@ -28,14 +30,30 @@ public class PlayerKeys : MonoBehaviour
     ///</param>
     void OnTriggerEnter(Collider other)
         {
-            // Check if it has the "Goal" tag
+            // Check if it has the "Keys" tag
             if (other.CompareTag("Keys"))
             {
                 numKeys++;
                 Debug.Log("Got Key");
                 //audio.PlayOneShot();
                 Destroy(other.gameObject);
-            }//end if("Goal")
+            }//end if("Keys")
+            else if (other.CompareTag("Doors"))
+            {
+                DoorMechanics doorData = other.gameObject.GetComponent<DoorMechanics>();
+                if (doorData != null && doorData.keysRequired <= numKeys)
+                {
+                    float counter = 0;
+                    Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+                    while (counter < openTime)
+                    {
+                        counter += Time.deltaTime;
+                        other.transform.position += Vector3.up * doorOpenSpeed * Time.deltaTime;
+
+                    }
+                }
+                
+            }//end if("Doors")
     
         }//end OnTriggerEnter()
      
