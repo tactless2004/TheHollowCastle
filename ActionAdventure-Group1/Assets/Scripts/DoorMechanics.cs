d936c7b799982d8b7e71925ff4ee65518a9e06ca
chaseacone <chasecone@proton.me>
2025-12-03 15:06:37 -0600

    Added Final door mecanics and fixed some bugs

--- ActionAdventure-Group1/Assets/Scripts/DoorMechanics.cs
+++ ActionAdventure-Group1/Assets/Scripts/DoorMechanics.cs
@@ -18,22 +18,36 @@ using UnityEngine;
 
 public class DoorMechanics : MonoBehaviour
 {
+    public GameManager gameManager;
     public int keysRequired = 5;
     [SerializeField] private float topHeight = 10.0f;
     [SerializeField] private float speed = 2.0f;
     public bool doorOpen = false;
-    
+    [SerializeField] private bool finalDoor = false;
+
+    void Awake()
+    {
+        if (!GameObject.FindGameObjectWithTag("GameManager").TryGetComponent(out gameManager))
+        {
+            Debug.LogError($"{name} could not find GameManager component.");
+        }
+    }
 
     void Update()
     {
-        if (doorOpen == true && transform.position.y < topHeight)
+        if (doorOpen == true && transform.position.y < topHeight && !finalDoor)
         {
             transform.position += new Vector3(0, Time.deltaTime * speed, 0);
         }
-        else if (doorOpen == true)
+        else if (doorOpen == true && !finalDoor)
         {
             Destroy(gameObject);
         }
+        else if (doorOpen == true && finalDoor)
+        {
+            gameManager.LoadNextLevel();
+        }
+        
     }
     
 }
