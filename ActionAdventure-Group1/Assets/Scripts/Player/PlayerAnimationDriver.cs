6dd15b571d62e6d1aa3454359147c16637405381
tactless2004 <leyton.mckinney@gmail.com>
2026-01-11 22:40:58 -0600

    feat: Add PlayerAnimationDriver to decouple animation from PlayerState in PlayerController

---
+++ ActionAdventure-Group1/Assets/Scripts/Player/PlayerAnimationDriver.cs
@@ -0,0 +1,73 @@
+/************************************************************
+* COPYRIGHT:  2026
+* PROJECT: TheHollowCastle
+* FILE NAME: PlayerAnimationDriver.cs
+* DESCRIPTION: Animation Driver with minor internal logic.
+*                   
+* REVISION HISTORY:
+* Date [YYYY/MM/DD] | Author | Comments
+* ------------------------------------------------------------
+* 2026/01/11 | Leyton McKinney | Init
+*
+************************************************************/
+
+using System;
+using UnityEngine;
+
+
+public class PlayerAnimationDriver : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+    
+    public bool IsMovementLocked { get; private set; }
+    public bool IsInAttack { get; private set; }
+    public bool IsDead { get; private set; }
+
+    public event Action onAttackCancelled;
+
+    public void TrySetMovement(bool isMoving)
+    {
+        if (IsMovementLocked || IsDead) return;
+        animator.Play(isMoving ? "Walk" : "Idle");
+    }
+
+    public bool TryPlayAttack(string animationName)
+    {
+        if (IsMovementLocked || IsDead) return false;
+
+        IsMovementLocked = true;
+        IsInAttack = true;
+
+        animator.Play(animationName);
+        return true;
+    }
+
+    public bool TryPlayDamage()
+    {
+        if (IsMovementLocked || IsDead) return false;
+
+        if (IsInAttack)
+        {
+            IsInAttack = false;
+            onAttackCancelled?.Invoke();
+        }
+
+        IsMovementLocked = true;
+
+        animator.Play("PlayerDamage");
+        return true;
+    }
+
+    public void PlayDeath()
+    {
+        IsDead = true;
+        IsMovementLocked = true;
+        animator.Play("PlayerDeath");
+    }
+
+    public void UnlockMovement()
+    {
+        IsMovementLocked = false;
+        IsInAttack = false;
+    }
+}
