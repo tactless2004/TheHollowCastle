5255c6b82ff246ff3443a14edd2822d6d9a9bbdf
tactless2004 <leyton.mckinney@gmail.com>
2026-01-11 22:14:00 -0600

    feat: add player context to avoid reference web between player components
    Now player components that need to be accessed will be access through the `PlayerContext` component.

---
+++ ActionAdventure-Group1/Assets/Scripts/Player/PlayerContext.cs
@@ -0,0 +1,25 @@
+/************************************************************
+* COPYRIGHT:  2026
+* PROJECT: TheHollowCastle
+* FILE NAME: PlayerContext.cs
+* DESCRIPTION: Player Context keeps track of Player Components and Events.
+*                   
+* REVISION HISTORY:
+* Date [YYYY/MM/DD] | Author | Comments
+* ------------------------------------------------------------
+* 2026/01/11 | Leyton McKinney | Init
+*
+************************************************************/
+ 
+using UnityEngine;
+ 
+
+public class PlayerContext : MonoBehaviour
+{
+    public PlayerController controller;
+    public PlayerMove move;
+    public PlayerInventory inventory;
+    public PlayerVitality vitality;
+    public PlayerKeys keys;
+    public PlayerWeaponSpawner weaponSpawner;
+}
